b8f32cb83d53c671a7900510ed16ddb112abbc92
nanakokomi <Jingtong.Cai21@xjtlu.edu.cn>
2026-05-15 23:16:25 +0100

    flask接口

---
+++ app2.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Flask"
+
+@app.route("/guest")
+def guest():
+    name=request.args.get("name", "Guest")
+    return f"Hello {name}"
+if __name__ == "__main__":
+    app.run(debug=True,port=5000,host="0.0.0.0")
+
+#app.run(debug=False, use_reloader=False)
